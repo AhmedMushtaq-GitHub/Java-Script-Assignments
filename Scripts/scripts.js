ad92b7894b00eb69294cf69ef6fbbc52d499fe4c
AhmedMushtaq-GitHub <you@example.com>
2023-09-27 00:04:54 -0700

    first commit

---
+++ Scripts/scripts.js
@@ -0,0 +1,50 @@
+console.log("Hello Ahmed");
+
+
+
+// let age = 25;
+// let age = 27;
+// let age = "25";
+// let age = "27";
+// var name = "Ahmed";
+// var name = "Ahmed";
+// const number = 25;
+// const number = 50;
+// const name = "Ahmed";
+// const name = "Ahmed";
+
+
+let Stu = ["Ahmed","ALi","Raza"]; //string Arry with Let
+
+for (let index = 0; index <= Stu.length; index++) {
+    
+    console.log(Stu[index]); //Console ma Show krwany k liey
+    document.write(Stu); //Screen pr show krwany k liwy
+    break;
+}
+document.write("<br>");//Line 
+
+
+const numbArr = [1,34,"Ali",true]; //String Array With Const
+numbArr.push('Ahmad qadri'); //Push Add New Element To The End Of String Array
+document.write("Arry is"+numbArr); //Length Of String Array
+
+document.write("<br>"); //Line break
+
+
+var Stu1 = ["Ahmed","ALi","Raza"]; //string Arry with Var
+
+for (var index = 0; index <= Stu.length; index++) {
+    
+    document.write(Stu1[2]); //Screen pr show krwany k liwy
+    break;
+} 
+document.write("<br>"); //Line break
+
+const Arry =[43,43,543,534,6,43643,64,36,346,43];
+for (let index = 0; index < Arry.length; index++) {
+document.write(Arry);
+break;
+    
+}
+
